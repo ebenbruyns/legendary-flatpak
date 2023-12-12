d2fecbb98afa850bcafc531f511438c812fe47c4
Eben Bruyns <eben@sdk.co.nz>
2023-12-13 09:45:55 +1300

    updated to reflect login fix

---
+++ create-flatpak.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/eben/work/legendary-flatpak
+
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pycairo
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' PyGObject
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' legendary-gl[webview_gtk]
+
+
+flatpak-builder legendary  com.github.derrod.legendary.yml --force-clean  
+flatpak-builder --user --install --force-clean legendary com.github.derrod.legendary.yml
+flatpak build-bundle ~/.local/share/flatpak/repo legendary.flatpak com.github.derrod.legendary
