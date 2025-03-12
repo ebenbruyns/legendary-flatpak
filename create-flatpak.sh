ef7c42d2083fdf3fca6f71e9ee9af9cf96e984e4
Eben Bruyns <eben@sdk.co.nz>
2025-03-13 11:19:10 +1300

    updated flatpak to build from github instead of pip

--- create-flatpak.sh
+++ create-flatpak.sh
@@ -2,10 +2,14 @@
 
 cd /home/eben/work/legendary-flatpak
 
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pycairo
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' PyGObject
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' legendary-gl[webview_gtk]
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pypatch
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' pycairo
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' PyGObject
+
+# ~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' pypatch
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' requests
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' pyinstaller
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' filelock     
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' altgraph 
 
 
 flatpak-builder legendary  com.github.derrod.legendary.yml --force-clean  
