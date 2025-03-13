10a7231915742662a0ca4ac735bd9ecd98898542
Eben Bruyns <eben@sdk.co.nz>
2025-03-13 15:37:00 +1300

    Fix broken web view

--- create-flatpak.sh
+++ create-flatpak.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 cd /home/eben/work/legendary-flatpak
+VERSION="24.08"
 
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' pycairo
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' PyGObject
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//${VERSION}" pycairo || exit
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//${VERSION}" PyGObject || exit
 
-# ~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' pypatch
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' requests
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' pyinstaller
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' filelock     
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//47' altgraph 
-
+# ~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//47' pypatch
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//${VERSION}" requests || exit
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//${VERSION}" pyinstaller || exit
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//${VERSION}" filelock || exit    
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//${VERSION}" altgraph || exit
+# newer versions are broken so don't regen this
+#~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime="org.freedesktop.Sdk//${VERSION}" pywebview || exit
 
 flatpak-builder legendary  com.github.derrod.legendary.yml --force-clean  
 flatpak-builder --user --install --force-clean legendary com.github.derrod.legendary.yml
